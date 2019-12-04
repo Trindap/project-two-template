5d70fc4d90ba473e8543f9ac7232e32fcbaa356e
Trindap <trinda@pdx.edu>
2019-12-04 12:09:34 -0800

    yup

--- scripts/script.js
+++ scripts/script.js
@@ -5,18 +5,25 @@ $(function() {
   x.classList.toggle("change");
 } 
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
 
-
-
-
-
-
-
-
-
-
-
-
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+});
 // End of Your Code . Don't delete that line below!!
 });
